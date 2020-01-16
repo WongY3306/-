b8af8d5318f01e825a538e7a6f4e4f9d43ec0aca
WongY3306 <2642630866@qq.com>
2020-01-16 17:59:58 +0800

    登陆注册前端

--- Backend/node/routes/sql.js
+++ Backend/node/routes/sql.js
@@ -1,10 +1,9 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
-    host: '152.136.113.170',
-    user: 'h5_1812',
-    password: 'db!H51812@tx',
-    database: 'H5_1812',
-    port: 3306,
+    host: '127.0.0.1', //ip地址
+	user: 'root', //数据库账号
+	password: 'root', //数据库密码
+	database: 'look' //数据库名
 });
 
 module.exports = {
--- Backend/node/routes/users.js
+++ Backend/node/routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var mySql = require('./sql')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+router.post('/login', (req, res) => {
+      var json = req.body
+      console.log(json)
+      mySql.sql({
+        sql: `insert into login (name,pass) values('${json.name}','${json.pass}')`,
+        // sql: 'insert into login (name,pass) values(?,?)',
+        // arr: [json.name, json.pass],
+        data(data) {
+          console.log(data)
+          res.send({
+            code: 0,
+            data: ''
+          })
+        }
+        // sql: `select * from login where user=1000`,
+        // data(data) {
+        //   console.log(data)
+        //   res.send({
+        //     code: 0,
+        //     data: ''
+        //   })
+        // }
+      })
+})
+      module.exports = router;
